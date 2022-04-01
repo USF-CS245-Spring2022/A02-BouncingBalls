a243d90a58c173830c6e907fde63e3f618338840
makobs <81383633+makobs@users.noreply.github.com>
2022-03-31 20:31:41 -0700

    seizure balls

--- src/A2/RollBounce.java
+++ src/A2/RollBounce.java
@@ -1,5 +1,5 @@
 // @author: Mia Kobayashi
-// @date and version: 30 Mar 2022, v.2
+// @date and version: 31 Mar 2022, v.6
 // CS245 Assignment 2: Roll Bounce
 
 package A2;
@@ -15,6 +15,12 @@ import java.io.IOException;
 import java.util.*;
 import java.util.List;
 import A2.ListNode;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Objects;
+import java.util.Properties;
+
 
 public class RollBounce extends JPanel implements ActionListener {
 
@@ -69,41 +75,45 @@ public class RollBounce extends JPanel implements ActionListener {
         windowWidth = 640;
         ballRadius = 20;
 
-//        allBalls = new ArrayList<Ball>();
-//
-//        for (int i = 0; i < balls; i++) { //create all balls
-//                allBalls.add(new Ball());
-//        }
-//
-//            for (Ball ball : allBalls) { //set starting coords & vels & color
-//                int x = random(0, windowWidth);
-//                int y = random(0, windowHeight);
-//
-//                int xV = random(minSpeed, maxSpeed);
-//                int yV = random(minSpeed, maxSpeed);
-//
-//                ball.setCoords(x, y);
-//                ball.setVels(xV, yV);
-//                ball.setColor(randColor());
-//            }
-//
-        b = new Ball();
+        allBalls = new ArrayList<Ball>();
 
-        int x = random(0, windowWidth);
-        int y = random(0, windowHeight);
+        for (int i = 0; i < balls; i++) { //create all balls
+                allBalls.add(new Ball());
+        }
 
-        int xV = random(minSpeed, maxSpeed);
-        int yV = random(minSpeed, maxSpeed);
+            for (Ball ball : allBalls) { //set starting coords & vels & color
+                int x = random(0, windowWidth);
+                int y = random(0, windowHeight);
 
-        b.setCoords(x, y);
-        b.setVels(xV, yV);
-        b.setColor(randColor());
+                int xV = random(minSpeed, maxSpeed);
+                int yV = random(minSpeed, maxSpeed);
 
+                ball.setCoords(x, y);
+                ball.setVels(xV, yV);
+                ball.setColor(randColor());
+            }
+////
+//        b = new Ball();
+//
+//        int x = random(0, windowWidth);
+//        int y = random(0, windowHeight);
+//
+//        int xV = random(minSpeed, maxSpeed);
+//        int yV = random(minSpeed, maxSpeed);
+//
+//        b.setCoords(x, y);
+//        b.setVels(xV, yV);
+//        b.setColor(randColor());
+//
         tm = new Timer(timerDelay, this);
 
 //        Properties p = new Properties();
 //        try {
-//            p.load(new FileInputStream(propertyFileName));
+//            ClassLoader classLoader = RollBounce.class.getClassLoader();
+//            URL res = Objects.requireNonNull(classLoader.getResource(propertyFileName), "Can't find file.");
+//            InputStream inputStream = new FileInputStream(res.getFile());
+//
+//            p.load(inputStream);
 //            gravity = (int) p.get("gravity");
 //            friction = (int) p.get("friction");
 ////            allBalls = (List<Ball>) Class.forName((String) p.get("list")).getDeclaredConstructor().newInstance();
@@ -141,7 +151,7 @@ public class RollBounce extends JPanel implements ActionListener {
 //                ball.setColor(randColor());
 //            }
 //
-//            tm = new Timer(timerDelay, this); // TODO: Replace the first argument with delay with value from config file.
+////            tm = new Timer(timerDelay, this); // TODO: Replace the first argument with delay with value from config file.
 //
 //        } catch (Exception e) {
 //            System.err.println("Property was not found, using default values.");
@@ -162,13 +172,13 @@ public class RollBounce extends JPanel implements ActionListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g); // Probably best you leave this as is.
 
-//        for (Ball b : allBalls) {
-//            g.setColor(b.getColor());
-//            g.fillOval(b.xCoord, b.yCoord, ballRadius, ballRadius);
-//        }
+        for (Ball b : allBalls) {
+            g.setColor(b.getColor());
+            g.fillOval(b.xCoord, b.yCoord, ballRadius, ballRadius);
+        }
 
-        g.setColor(b.getColor());
-        g.fillOval(b.xCoord, b.yCoord, ballRadius, ballRadius);
+//        g.setColor(b.getColor());
+//        g.fillOval(b.xCoord, b.yCoord, ballRadius, ballRadius);
 
         // Recommend you leave the next line as is
         tm.start(); //then all the actionPerformed stuff happens
@@ -178,47 +188,62 @@ public class RollBounce extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-//        for (Ball b : allBalls) {
+        for (Ball b : allBalls) {
+            System.out.println("vels: " + b.xVel + " " + b.yVel);
             if (b.xCoord <= ballRadius || b.xCoord >= windowWidth - ballRadius) { //hits left or right
-                b.xVel = -(b.xVel - friction); //change the vel
+                b.xVel = -(b.xVel - friction); //change vel
             }
             if (b.yCoord <= ballRadius || b.yCoord >= windowHeight - ballRadius) { //hits top or bottom
-                b.yVel = -(b.yVel - friction); //change the vel
+                b.yVel = -(b.yVel - friction); //change vel
             }
 
-            if (b.xCoord + b.xVel >= windowWidth) {//reaches right side
+
+
+            if (b.xCoord + b.xVel >= windowWidth) {//hits right
                 b.xCoord = windowWidth - ballRadius;
-            }
-            else {
+            } else {
                 b.xCoord += b.xVel; //move horz
+                if (b.xCoord > windowWidth - ballRadius) {
+                    b.xCoord = windowWidth - ballRadius;
+                }
             }
 
-            b.yVel += gravity; //continue to drop ball
 
-            if (b.yCoord + b.yVel >= windowHeight) { //reaches bottom
+            if (!(b.yCoord > windowHeight - ballRadius)) { //apply gravity only if ball is not touching bottom
+                b.yVel += gravity; //continue to drop ball (in its direction)
+            }
+
+
+            //fixme
+            if ((b.yVel >= -friction && b.yVel <= friction) && b.yCoord == windowHeight - ballRadius) { //at some point, the yVel needs to become 0 & x
+                b.yVel = 0;
+                System.out.println("coord: " + b.xCoord + " " + b.yCoord);
+                System.out.println("yVel becomes zero");
+            }
+
+            if (b.yCoord + b.yVel >= windowHeight - ballRadius) { //if moving the ball hits bottom
                 b.yCoord = windowHeight - ballRadius;
                 if (b.xVel != 0) {//if ball still rolling, keep it moving
                     b.xVel += b.xVel > 0 ? -friction : friction;
-                }
-                else {
+                } else {
                     b.xVel = 0;
                 }
-            } else {
+
+            } else { //continue to move ball in direction of travel
                 b.yCoord += b.yVel; //move vert
+                if (b.yCoord > windowHeight - ballRadius) {
+                    b.yCoord = windowHeight - ballRadius;
+                }
             }
 
-//            if (b.xVel <= 2*friction && b.xVel >= -friction*2) {
-//                if (b.yVel <= 2*friction && b.yVel >= -friction*2) {
-//                    System.out.println("pls stop");
-//                }
-//            }
+//        System.out.println(b.xVel);
 
-            System.out.println("---------------" + b);
-            System.out.println("coord: " + b.xCoord + " " + b.yCoord);
-            System.out.println("vels: " + b.xVel + " " + b.yVel);
+//            System.out.println("---------------");
+//            System.out.println("coord: " + b.xCoord + " " + b.yCoord);
+//            System.out.println("vels: " + b.xVel + " " + b.yVel);
 
             repaint();
-//        }
+        } //end for
     } //end actionPerformed()
 
 
@@ -247,7 +272,7 @@ public class RollBounce extends JPanel implements ActionListener {
 
         JFrame jf = new JFrame();
         jf.setTitle("Roll Bounce");
-        jf.setSize(windowWidth, windowHeight + 50); //640x480
+        jf.setSize(windowWidth, windowHeight + (ballRadius * 2)); //640x480
         jf.add(rb);
 //        jf.getRootPane().setBorder(BorderFactory.createMatteBorder(windowWidth, windowHeight, windowWidth, windowHeight, Color.BLACK));
         jf.setVisible(true);
